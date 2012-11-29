c6e850a4af6fa4c3c4d78ff2ddaf1085717f6005
Michael Zavarella <mzavarella30@gmail.com>
2012-11-28 21:53:08 -0500

    More Movement

--- Movement.js
+++ Movement.js
@@ -1,160 +1,39 @@
 // Movement.js
 
-// Move the player north
-function move_north() {
-	usermoves = usermoves + 1;
-	switch(userlocation) {
-		case 4  : userlocation = 1
-				  break;
-		case 8  : userlocation = 4
-				  break;
-		case 9  : userlocation = 8
-				  break;
-		case 11 : userlocation = 17
-				  break;
-		case 15 : userlocation = 14
-				  break;
-		case 17 : userlocation = 19
-				  break;
-		case 20 : userlocation = 9
-				  break;
-		case 21 : if (payment === true) {
-				      userlocation = 20
-				  } else {
-				  		userlocation = 21
-				  }
-				  break;
-		case 22 : userlocation = 21
-				  break;
-		case 24 : userlocation = 16
-				  break;
-		default : var msg = "You cannot go that way silly!"
-				  updateText(msg);
-				  break;
+var GameArea = new Array(
+				 /*  N   S   E   W */
+				 /*  0   1   2   3 */
+		/* 0 */    [-1, -1, -1,  1],
+		/* 1 */    [-1,  4,  0,  2],
+		/* 2 */    [-1, -1,  1, -1],
+		/* 3 */    [-1, -1, -1,  4],
+		/* 4 */    [ 1,  8,  3,  5],
+		/* 5 */    [-1, -1,  4,  6],
+		/* 6 */    [-1, -1,  5, -1],
+		/* 7 */    [-1, -1,  8, -1],
+		/* 8 */    [ 4, -1, 10,  7],
+		/* 9 */    [13, 17, -1, -1],
+		/* 10 */   [-1, -1, 11,  8],
+		/* 11 */   [14, -1, 12, 10],
+		/* 12 */   [-1, -1, 13, 11],
+		/* 13 */   [-1,  9, -1, 12],
+		/* 14 */   [16, 11, 15, -1],
+		/* 15 */   [-1, -1, -1, 14],
+		/* 16 */   [-1, 14, -1, -1],
+		/* 17 */   [ 9, 18, -1, -1],
+		/* 18 */   [17, 19, 20, -1],
+		/* 19 */   [18, -1, -1, -1],
+		/* 20 */   [-1, -1, -1, 18]
+			    );	
+			    				
+function move(direction) {
+	newLocation = GameArea[userlocation][direction];
+	if (newLocation !== -1) {
+		userlocation = newLocation;
+		updateText(ListOfLocations[userlocation])
+		ButtonSwitchFunction(userlocation);
+	} else if (newLocation === -1) {
+		var msg = "You can't go that way silly!"
+		updateText(msg);
 	}
-	updateText(ListOfLocations[userlocation]);
-	button_switch(userlocation);
-}
-
-// Move the player south
-function move_south() {
-	usermoves = usermoves + 1;
-	switch(userlocation) {
-		case 1  : userlocation = 4
-				  break;
-		case 4  : userlocation = 8
-				  break;
-		case 8  : if (Inventory.length === 2) {
-				  userlocation = 9
-				  } else {
-				  	var msg = "You cannot go that way yet";
-				  	updateText(msg);
-				  	userlocation = 8;
-				  }
-				  break;
-		case 9  : userlocation = 20
-				  break;
-		case 14 : userlocation = 15;
-				  break;
-		case 16 : userlocation = 24
-				  break;
-		case 17 : userlocation = 11
-				  break;
-		case 19 : userlocation = 17
-				  break;
-		case 20 : userlocation = 21
-				  break;
-		case 21 : if (cookies === true) {
-				  	  userlocation = 22
-				  } else {
-				  	    var msg = "You have nothing to pay for."
-				  	    updateText(msg)
-				  }
-				  break;
-		default : var msg = "You cannot go that way silly!"
-				  updateText(msg);
-				  break;
-	}
-	updateText(ListOfLocations[userlocation]);
-	button_switch(userlocation);
-}
-
-// Move the player east
-function move_east() {
-	usermoves = usermoves + 1;
-	switch(userlocation) {
-		case 1  : userlocation = 0
-				  break;
-		case 2  : userlocation = 1
-				  break;
-		case 4  : userlocation = 3
-				  break;
-		case 5  : userlocation = 4
-				  break;
-		case 6  : userlocation = 5
-				  break;
-		case 7  : userlocation = 8
-				  break;
-		case 8  : userlocation = 10
-				  break;
-		case 10 : userlocation = 11
-				  break;
-		case 11 : userlocation = 12
-				  break;
-		case 12 : userlocation = 13
-				  break;
-		case 13 : userlocation = 14
-				  break;
-		case 15 : userlocation = 16
-				  break;
-		case 17 : userlocation = 18
-				  break;
-		case 21 : userlocation = 23
-				  break;
-		default : var msg = "You cannot go that way silly!"
-				  updateText(msg);
-				  break;
-	}
-	updateText(ListOfLocations[userlocation]);
-	button_switch(userlocation);
-}
-
-// Move the player west
-function move_west() {
-	usermoves = usermoves + 1;
-	switch(userlocation) {
-		case 0  : userlocation = 1
-			      break;
-		case 1  : userlocation = 2
-				  break;
-		case 3  : userlocation = 4
-				  break;
-		case 4  : userlocation = 5
-				  break;
-		case 5  : userlocation = 6
-				  break
-		case 8  : userlocation = 7
-				  break;
-		case 10 : userlocation = 8
-				  break;
-		case 11 : userlocation = 10
-				  break;
-		case 12 : userlocation = 11
-				  break;
-		case 13 : userlocation = 12
-				  break;
-		case 14 : userlocation = 13
-				  break;
-		case 16 : userlocation = 15
-				  break;
-		case 18 : userlocation = 17
-				  break;
-		case 23 : userlocation = 21
-				  break;
-		default : var msg = "You cannot go that way silly!"
-				  updateText(msg);
-				  break;
-	}
-	updateText(ListOfLocations[userlocation]);
-	button_switch(userlocation);
 }
